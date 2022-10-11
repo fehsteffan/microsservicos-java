6a29480124dd4089c840fb2f883d6ab747246c72
Felipe Steffan <felip.developer@hotmail.com>
2022-10-11 14:41:16 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/program/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
