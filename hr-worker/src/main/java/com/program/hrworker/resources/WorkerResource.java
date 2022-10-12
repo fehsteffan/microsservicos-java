e22ce7cc90eef7364c4291abac1ae7c9d4df27c0
Felipe Steffan <felip.developer@hotmail.com>
2022-10-12 19:11:28 -0300

    db config server

--- hr-worker/src/main/java/com/program/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/program/hrworker/resources/WorkerResource.java
@@ -24,9 +24,7 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("{test.config}")
-	private String testConfig;
-	
+		
 	
 	@Autowired
 	private Environment env;
@@ -37,7 +35,7 @@ public class WorkerResource {
 	
 	@GetMapping(value="/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
@@ -51,18 +49,18 @@ public class WorkerResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		
+		/*
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {			
 			e.printStackTrace();
 		}
-		
+		*/
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
-
+	
 }
